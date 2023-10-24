f775838a3bdc14ab56d763903f4d8da9e8ff2978
xodnd0507 <24457545yong@gmail.com>
2023-10-24 21:31:09 +0900

    Feat : instagram basic disign

---
+++ index.js
@@ -0,0 +1,7 @@
+// 이미지슬라이드 페이드 인 페이드 아웃
+$('.imgSlide a:gt(0)').hide();
+
+setInterval(function(){
+$('.imgSlide a:first-child').fadeOut()
+.next('a').fadeIn().end().appendTo('.imgSlide');
+}, 3000);
